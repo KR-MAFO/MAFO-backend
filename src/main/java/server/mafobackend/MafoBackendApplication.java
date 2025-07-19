ce8f9a688cab40e464e8e15cb01abde981a03eef
beargame <hoyoung7827@gmail.com>
2025-07-19 12:54:26 +0900

    설정

---
+++ src/main/java/server/mafobackend/MafoBackendApplication.java
@@ -0,0 +1,13 @@
+package server.mafobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MafoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MafoBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/server/mafobackend/MafoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package server.mafobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MafoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
